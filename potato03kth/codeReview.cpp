13745067e946f7ff29935ad7c6f52b512e945fc2
potato03kth <potatokth@gmail.com>
2024-03-25 10:23:29 +0900

    move the codeReview.cpp

---
+++ potato03kth/codeReview.cpp
@@ -0,0 +1,47 @@
+대한민국 육군 학생군사교육단 159학군단 64기 사관후보생 이병헌
+{
+
+    대한민국.육군.학생군사교육단 .159학군단.64기.this = let new (이병헌);
+}
+
+namespace 64기 {
+    64기 ::사관후보생}
+
+이병헌 = 64기 ::사관후보생(std::name_ptr<이병헌>)
+
+    탬플릿이 무엇이냐
+    ? 탬플릿이란,
+    "<syntex>"의 꼴로 사용하는, 연산자 ?, ㅏ
+
+                                              대상명<자료형>
+                                                  에서, 자료형에 따라, ㅁㄴㅇㄹㅇㄹㅁㄴㄹㄴㅁㅇ한다고 ? ㅓ 어떤 list가 아루,
+
+    list의 이름이 "메모리의 시작 주소 ptr"
+
+    과일 = [];
+
+vector 는 배열이다.가변길이배열
+        linked list의 시작주소라고
+    ?
+
+    pipeline(shared_ptr<353> ctx)
+    : _ctx(ctx),
+    _dispatcher(10),
+    _hub(ctx, RS2_PRODUCT_LINE_ANY_INTEL),
+    _synced_streams({RS2_STREAM_COLOR, RS2_STREAM_DEPTH, RS2_STREAM_INFRARED, RS2_STREAM_FISHEYE})
+{
+}
+
+// 주소는 도로명 주솓도 있고 집주소도 잇어요
+//  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+template <typename 주소>
+주소 주소변환(주소 a) :
+
+                        pipeline::pipeline(std::shared_ptr<librealsense::context> ctx) : _ctx(ctx),
+                                                                                         _dispatcher(10),
+                                                                                         _hub(ctx, RS2_PRODUCT_LINE_ANY_INTEL),
+                                                                                         _synced_streams({RS2_STREAM_COLOR, RS2_STREAM_DEPTH, RS2_STREAM_INFRARED, RS2_STREAM_FISHEYE}){}
+
+                                                                                             .pipeline(shared_ptr<arstarstarstartsr> ctx)
+
+                                                                                                 대한민국.육군.학생군사교육단 .159학군단.64기.this = let new(이병헌);
